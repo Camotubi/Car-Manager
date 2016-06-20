29cd878b5aaf1a0184fced71d572a28a98b15301
Carlos Tuñón <camotubi@gmail.com>
2016-06-19 23:10:38 -0500

    Add id to Car, Improve saving and importing of cars from  file (Broken)

--- Car.h
+++ Car.h
@@ -1,23 +1,28 @@
+//Include Guard
 #ifndef __CAR_H_INCLUDED__
 #define __CAR_H_INCLUDED__
 using namespace std;
+//Dependencies
+#include <vector>
 class Car
 {
+	
 	private:
 	string nombre;
-	int ano;
+	int ano,id;
 	vector<float> fuel_economy;
 public:
-	Car(string nombre, int ano)
+	Car(string nombre, int ano, int id)
 	{
 		this->nombre = nombre;
 		this->ano = ano;
+        this->id = id;
 	}
-	string getnombre()
+	string get_nombre()
 	{
 		return nombre;
 	}
-	int getano()
+	int get_ano()
 	{
 		return ano;
 	}
@@ -25,11 +30,24 @@ public:
 	{
 		return fuel_economy.back();
 	}
+    float getany_fuel_economy(int n)
+    {
+        return fuel_economy.at(n);
+    }
+    int get_fuel_economy_size()
+    {
+        return fuel_economy.size();
+    }
+    int get_id()
+    {
+        return id;
+    }
 	float calculate_fuel_economy(float fuel_final, float fuel_inicial,float distance)
 	{
 		return (fuel_final-fuel_inicial)/distance;
 	}
 
+
 };
-#endif
+#endif // __CAR_H_INCLUDED
 	
--- main.cpp
+++ main.cpp
@@ -4,15 +4,21 @@
 #include <vector>
 #include <cstdlib>
 #include "Car.h"
-#define MENU "a) Crear Nuevo Auto \nb) Mostrar Autos en Memoria\nc) Guardar Vehiculos en Memoria\nd) Cargar Autos a memoria\nf) Limpiar Pantalla"
+#include <string>
+#define MENU "a) Crear Nuevo Auto \nb) Mostrar Autos en Memoria\nc) Guardar Vehiculos en Memoria\nd) Cargar Autos a memoria\nf) Limpiar Pantalla\ng) Cerrar Programa"
 
 using namespace std;
 int main () {
 	vector<Car> carros;
 	string carro_nombre;
+    fstream myfiles;
 	int carro_ano;
+    int carro_id;
 	char menu_op;
+    char yes_no;
+    
 	void printMenu();
+    string nom_archivo;
 	while(true)
 	{
 		printMenu();
@@ -24,54 +30,96 @@ int main () {
 				cin>>carro_nombre;
 				cout<<"Ingrese año del Vehiculo"<<endl;
 				cin>>carro_ano;
-				carros.push_back(Car(carro_nombre,carro_ano));
+				carros.push_back(Car(carro_nombre,carro_ano,carro_id));
 			break;
 			
 			case 'b':
-				for(int i =0; i<carros.size();i++)
+				for(int unsigned i =0; i<carros.size();i++)
 				{
-					cout<<carros.at(i).getnombre()<<endl;
+					cout<<carros.at(i).get_nombre()<<endl;
 				}
 			break;
 			
 			case 'c':
 				{
-					ofstream datos;
-					datos.open("data.txt");
+                cout<<"Ingrese el nombre del archivo"<<endl;
+                cin>>nom_archivo;
+					myfiles.open(nom_archivo.c_str(),fstream::out);
 					for(int unsigned n=0; n<carros.size(); n++)
 					{
-						datos<<carros.at(n).getnombre()<<endl;
+						myfiles<<carros.at(n).get_nombre()<<endl;
 					}
-					datos.close();
+					myfiles.close();
 				}
 				
 			break;
 			
 			case 'd':
 			{
-				ifstream datos;
-				datos.open("data.txt");
-				while(getline(datos,carro_nombre))
+                if(carros.size()>0)
+                {
+                    cout<<"Hay autos en memoria." 
+                    "\n¿Desea Guardar los Autos en otro lado antes de importar los nuevos?"
+                    "\n(s/n)( Los autos en memoria seran reemplazados )\n";
+                    cin>>yes_no;
+                    if(yes_no == 's')
+                    {
+                        cout<<"Nombre del Archivo:";
+                        cin>>nom_archivo;
+                        myfiles.open(nom_archivo.c_str(), fstream::out);
+                        for(int unsigned n=0; n<carros.size(); n++)
+                        {
+                            myfiles<<carros.at(n).get_id();
+                            myfiles<<"{";
+                            myfiles<<carros.at(n).get_nombre()<<";";
+                            myfiles<<"Nombre:"<<carros.at(n).get_nombre()<<";";
+                            myfiles<<"Año:"<<carros.at(n).get_ano()<<";";
+                            myfiles<<"ID:"<<carros.at(n).get_id()<<";";
+                            if(carros.at(n).get_fuel_economy_size()!=0)
+                            {
+                                myfiles<<"Fuel Economy:";
+                                for(int unsigned i = 0; i < carros.at(n).get_fuel_economy_size() - 1; i++)
+                                {
+                                    myfiles<<carros.at(n).getany_fuel_economy(i)<<",";
+                                }
+                                myfiles<<carros.at(n).getcurrent_fuel_economy()<<";";
+                            }
+                            
+                        }
+                    myfiles.close();
+                    }
+                        
+                }
+            }
+                cout<<"Ingrese el nombre del archivo"<<endl;
+                cin>>nom_archivo;
+				myfiles.open(nom_archivo.c_str(),fstream::in);
+				while(getline(myfiles,carro_nombre))
 				{
-					carros.push_back(Car(carro_nombre,1));
+					carros.push_back(Car(carro_nombre,1,1));
 				}
-				datos.close();
-			}
-			
+				myfiles.close();
 			break;
 			
 			case 'f':
 				system("clear");
 			break;
+            
+            case 'g':
+                return 0;
 		}
 		
 	}
-	
-	
-  return 0;
 }
 
 
-void printMenu(){
-			cout<<MENU<<endl;
+void printMenu()
+{
+    cout<<MENU<<endl;
+}
+
+bool saveCars()
+{
+    
+    return true;
 }
